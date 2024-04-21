1ad8b921d46e7854ff36980758c289f7be09eb04
Mike E <mike@greenphoenixengineering.com>
2024-04-21 17:44:52 -0500

    adds reverse words

--- co/stripe.go
+++ co/stripe.go
@@ -1,5 +1,7 @@
 package co
 
+import "strings"
+
 // HasTwoSum searches for sums by checking for the complementary difference with - values in the array and target
 func HasTwoSum(nums []int, target int) bool {
 	diffMap := make(map[int]bool)
@@ -51,4 +53,53 @@ func PinpointZero(matrix [][]int) [][]int {
 	}
 
 	return matrix
+}
+
+type Node struct {
+	Val int
+	Next *Node
+}
+
+func SumLinkedLists(l1, l2 *Node) *Node {
+	summed := &Node{}
+	current := summed
+	carryOver := 0
+
+	for l1 != nil || l2 != nil || carryOver > 0 {
+		sum := carryOver
+
+		if l1 != nil {
+			sum += l1.Val
+			l1 = l1.Next
+		}
+
+		if l2 != nil {
+			sum += l2.Val
+			l2 = l2.Next
+		}
+
+		carryOver = sum / 10
+		current.Next = &Node{Val: sum % 10}
+		current = current.Next
+	}
+
+	return current.Next
+}
+
+func ReverseWords (s []byte) string {
+	// convert byte slice to string
+	str := string(s)
+
+	// split string on spaces
+	words := strings.Fields(str)
+
+	// swap
+	for i, j := 0, len(words)-1; i < j; i, j = i+1, j-1 {
+		words[i], words[j] = words[j], words[i]
+	}
+
+	// create resulting string from reversed slice
+	reversed := strings.Join(words, " ")
+
+	return reversed
 }
--- main.go
+++ main.go
@@ -9,11 +9,14 @@ import (
 func main() {
 	fmt.Println("----fizz buzz----: ")
 	fmt.Println(fizzBuzz(20))
+
 	fmt.Println("----palidrome----: ")
 	fmt.Println(isPalindrome("palidrome"))
 	fmt.Println(isPalindrome("racecar"))
+
 	fmt.Println("----Has Two Sums----: ")
 	fmt.Println(co.HasTwoSum([]int{2, 7, 11, 15}, 9))
+
 	fmt.Println("----pin point zeros in matrix----: ")
 	fmt.Println(co.PinpointZero([][]int{
 		{1, 2, 3, 4},
@@ -21,6 +24,14 @@ func main() {
 		{9, 10, 11, 12},
 		{13, 14, 15, 0},
 	}))
+
+	fmt.Println("----sum linked lists----: ")
+	l1 := &co.Node{Val: 2, Next: &co.Node{Val: 4, Next: &co.Node{Val: 3}}}
+	l2 := &co.Node{Val: 5, Next: &co.Node{Val: 6, Next: &co.Node{Val: 4}}}
+	fmt.Println(co.SumLinkedLists(l1, l2))
+
+	fmt.Println("----reverse words----: ")
+	fmt.Println(co.ReverseWords([]byte("reverse me")))
 }
 
 func fizzBuzz(n int) []string {
